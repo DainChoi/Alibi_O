a892ce259b7367998389e9db23c12cba4deddf29
DainChoi <8677k@naver.com>
2021-05-04 14:07:14 +0900

    bottom navi

--- app/src/main/java/com/dproject/alibi_o/WorkActivity.java
+++ app/src/main/java/com/dproject/alibi_o/WorkActivity.java
@@ -18,6 +18,8 @@ public class WorkActivity extends AppCompatActivity {
     private Frag1 frag1;
     private Frag2 frag2;
     private Frag3 frag3;
+    private Frag4 frag4;
+    private Frag5 frag5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +31,20 @@ public class WorkActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
-                    case R.id.action_record:
+                    case R.id.action_today:
                         setFrag(0);
                         break;
-                    case R.id.action_salary:
+                    case R.id.action_record:
                         setFrag(1);
                         break;
-                    case R.id.action_profile:
+                    case R.id.action_schedule:
                         setFrag(2);
+                        break;
+                    case R.id.action_salary:
+                        setFrag(3);
+                        break;
+                    case R.id.action_profile:
+                        setFrag(4);
                         break;
                 }
                 return true;
@@ -45,6 +53,8 @@ public class WorkActivity extends AppCompatActivity {
         frag1 = new Frag1();
         frag2 = new Frag2();
         frag3 = new Frag3();
+        frag4 = new Frag4();
+        frag5 = new Frag5();
         setFrag(0); // 첫 프래그먼트 화면을 무엇으로 지정해줄 것인지 선택
     }
 
@@ -63,6 +73,14 @@ public class WorkActivity extends AppCompatActivity {
                 break;
             case 2:
                 ft.replace(R.id.main_frame, frag3);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag4);
+                ft.commit();
+                break;
+            case 4:
+                ft.replace(R.id.main_frame, frag5);
                 ft.commit();
                 break;
 
