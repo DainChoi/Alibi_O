1c98d041ffc8b5fc025e20e31ac534e255fb411c
DainChoi <8677k@naver.com>
2021-05-26 10:49:37 +0900

    modify 오류 수정 전

--- app/src/main/java/com/dproject/alibi_o/ModifyActivity.java
+++ app/src/main/java/com/dproject/alibi_o/ModifyActivity.java
@@ -2,7 +2,10 @@ package com.dproject.alibi_o;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ModifyActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ModifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify);
+
+        Button btn_save = findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ModifyActivity.this, Frag5.class);
+                startActivity(intent); // 오류
+                finish();
+            }
+        });
     }
 }
