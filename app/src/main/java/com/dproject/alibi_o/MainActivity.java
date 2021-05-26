3be7e8b74031c35d1f8473e19f47b5b23c5cc06d
Kangeuni <49019214+Kangeuni@users.noreply.github.com>
2021-05-26 13:50:37 +0900

    알바main = 사장main

--- app/src/main/java/com/dproject/alibi_o/MainActivity.java
+++ app/src/main/java/com/dproject/alibi_o/MainActivity.java
@@ -1,32 +1,57 @@
 package com.dproject.alibi_o;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-   
+
+
+    RecyclerView recycle1;
+    RecycleAdapter adapter;
+
+    int imgID[] = {R.drawable.logo, R.drawable.logo, R.drawable.logo, R.drawable.logo, R.drawable.logo,
+            R.drawable.logo, R.drawable.logo, R.drawable.logo, R.drawable.logo, R.drawable.logo};
+
+    String title[] = {"logo1", "logo2", "logo3", "logo4", "logo5", "logo6", "logo7", "logo8", "logo9", "logo10"};
+
+    String content[] = {"test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton btn_setting = findViewById(R.id.btn_setting);
-        btn_setting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        recycle1 = findViewById(R.id.recycle1);
 
-                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
-                startActivity(intent);
-                finish();
+        adapter = new RecycleAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recycle1.setLayoutManager(layoutManager);
+
+        for (int i = 0; i < imgID.length; i++){
+            adapter.addItem(new ItemData(imgID[i],title[i],content[i]));
+        }
+
+        recycle1.setAdapter(adapter);
+
+        adapter.setOnItemClickListner(new OnItemDataClickListener() {
+
+            public void OnItemClick(RecycleAdapter.ViewHolder holder, View view, int position) {
+                ItemData data = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),data.title+"가 선택됨",Toast.LENGTH_SHORT).show();
+
             }
         });
 
---
+++ app/src/main/java/com/dproject/alibi_o/OnItemDataClickListener.java
@@ -0,0 +1,8 @@
+package com.dproject.alibi_o;
+
+import android.view.View;
+
+public interface OnItemDataClickListener {
+    public void OnItemClick(RecycleAdapter.ViewHolder holder, View view, int position);
+
+}
