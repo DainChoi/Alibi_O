0fda9fbdfc427cb048c243cdab8f4257f1e76eba
DainChoi <8677k@naver.com>
2021-06-23 15:07:29 +0900

    frag4 수정

---
+++ app/src/main/java/com/dproject/alibi_o/AddActivity.java
@@ -0,0 +1,57 @@
+package com.dproject.alibi_o;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        ImageButton btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 오류
+                //Intent intent = new Intent(AddActivity.this, Frag4.class);
+                //startActivity(intent);
+                //finish();
+            }
+        });
+    }
+
+/*
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_add, container, false);
+
+        ImageButton btn_back = (ImageButton) view.findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // AddActivity로 이동
+                Intent intent = new Intent(getActivity(), Frag4.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }*/
+}
--- app/src/main/java/com/dproject/alibi_o/Frag4.java
+++ app/src/main/java/com/dproject/alibi_o/Frag4.java
@@ -1,13 +1,17 @@
 package com.dproject.alibi_o;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Frag4 extends Fragment {
 
@@ -17,6 +21,16 @@ public class Frag4 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag4, container, false);
+        FloatingActionButton btn_add = (FloatingActionButton) view.findViewById(R.id.btn_add);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // AddActivity로 이동
+                Intent intent = new Intent(getActivity(), AddActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
