334db74f640f126dc921a41a3f70b5d24e395b3e
YongSeok <kus3860@naver.com>
2021-08-18 00:06:11 +0900

    CalendarView DayChange

--- app/src/main/java/com/dproject/alibi_o/Frag2.java
+++ app/src/main/java/com/dproject/alibi_o/Frag2.java
@@ -5,15 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
+import java.text.BreakIterator;
+
 public class Frag2 extends Fragment {
 
     private View view;
+
 
     @Nullable
     @Override
@@ -21,6 +30,7 @@ public class Frag2 extends Fragment {
         view = inflater.inflate(R.layout.frag2, container, false);
 
         ImageButton btn_notify = (ImageButton) view.findViewById(R.id.btn_notify);
+        
         btn_notify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,7 +41,17 @@ public class Frag2 extends Fragment {
                 startActivity(intent);
             }
         });
+        TextView whenDate = (TextView) view.findViewById(R.id.textView);
+        CalendarView calendarView = (CalendarView) view.findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // todo
+                String date = year + "/" + (month + 1) + "/" + dayOfMonth;
+                whenDate.setText(date); // 선택한 날짜로 설정
 
+            }
+        });
         return view;
     }
 }
