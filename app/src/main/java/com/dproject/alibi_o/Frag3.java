1ac40cb6574a7a3b2edc4742ed52be1c05fa2723
DainChoi <8677k@naver.com>
2021-09-21 21:22:17 +0900

    schedule db

--- app/src/main/java/com/dproject/alibi_o/Frag3.java
+++ app/src/main/java/com/dproject/alibi_o/Frag3.java
@@ -1,29 +1,90 @@
 package com.dproject.alibi_o;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.shrikanthravi.collapsiblecalendarview.data.Day;
 import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
 
 public class Frag3 extends Fragment {
 
     private View view;
+    EditText name_input;
+    EditText sun_in_input, mon_in_input, tue_in_input, wed_in_input, thu_in_input, fri_in_input, sat_in_input;
+    EditText sun_out_input, mon_out_input, tue_out_input, wed_out_input, thu_out_input, fri_out_input, sat_out_input;
+    ImageButton btn_check;
+    DatabaseReference databaseReference;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag3, container, false);
+
+        name_input = view.findViewById(R.id.name_input);
+        sun_in_input = view.findViewById(R.id.sun_in);
+        mon_in_input = view.findViewById(R.id.mon_in);
+        tue_in_input = view.findViewById(R.id.tue_in);
+        wed_in_input = view.findViewById(R.id.wed_in);
+        thu_in_input = view.findViewById(R.id.thu_in);
+        fri_in_input = view.findViewById(R.id.fri_in);
+        sat_in_input = view.findViewById(R.id.sat_in);
+
+        sun_out_input = view.findViewById(R.id.sun_out);
+        mon_out_input = view.findViewById(R.id.mon_out);
+        tue_out_input = view.findViewById(R.id.tue_out);
+        wed_out_input = view.findViewById(R.id.wed_out);
+        thu_out_input = view.findViewById(R.id.thu_out);
+        fri_out_input = view.findViewById(R.id.fri_out);
+        sat_out_input = view.findViewById(R.id.sat_out);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Alibi").child("time");
+
+        btn_check = view.findViewById(R.id.btn_check);
+        btn_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = name_input.getText().toString();
+                String sun_in = sun_in_input.getText().toString();
+                String mon_in = mon_in_input.getText().toString();
+                String tue_in = tue_in_input.getText().toString();
+                String wed_in = wed_in_input.getText().toString();
+                String thu_in = thu_in_input.getText().toString();
+                String fri_in = fri_in_input.getText().toString();
+                String sat_in = sat_in_input.getText().toString();
+
+                String sun_out = sun_out_input.getText().toString();
+                String mon_out = mon_out_input.getText().toString();
+                String tue_out = tue_out_input.getText().toString();
+                String wed_out = wed_out_input.getText().toString();
+                String thu_out = thu_out_input.getText().toString();
+                String fri_out = fri_out_input.getText().toString();
+                String sat_out = sat_out_input.getText().toString();
+
+                Time time = new Time(name, sun_in, mon_in, tue_in, wed_in, thu_in, fri_in, sat_in,
+                        sun_out, mon_out, tue_out, wed_out, thu_out, fri_out, sat_out);
+                // databaseReference.push().setValue(work);
+                databaseReference.child(time.getName()).setValue(time);
+                Toast.makeText(getActivity(), "스케줄이 등록되었습니다!", Toast.LENGTH_SHORT).show();
+
+                //  Intent intent = new Intent(WorkaddActivity.this, MainActivity.class);
+                //  startActivity(intent);
+                //  finish();
+            }
+        });
+
 
         final CollapsibleCalendar collapsibleCalendar = view.findViewById(R.id.calendarView);
         collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
---
+++ app/src/main/java/com/dproject/alibi_o/Time.java
@@ -0,0 +1,154 @@
+package com.dproject.alibi_o;
+
+public class Time {
+
+    private String name;
+    private String sun_in;
+    private String mon_in;
+    private String tue_in;
+    private String wed_in;
+    private String thu_in;
+    private String fri_in;
+    private String sat_in;
+
+    private String sun_out;
+    private String mon_out;
+    private String tue_out;
+    private String wed_out;
+    private String thu_out;
+    private String fri_out;
+    private String sat_out;
+
+
+    public Time(String name, String sun_in, String mon_in, String tue_in,
+                String wed_in, String thu_in, String fri_in, String sat_in,
+                String sun_out, String mon_out, String tue_out,
+                String wed_out, String thu_out, String fri_out, String sat_out) {
+
+        this.name = name;
+        this.sun_in = sun_in;
+        this.mon_in = mon_in;
+        this.tue_in = tue_in;
+        this.wed_in = wed_in;
+        this.thu_in = thu_in;
+        this.fri_in = fri_in;
+        this.sat_in = sat_in;
+
+        this.sun_out = sun_out;
+        this.mon_out = mon_out;
+        this.tue_out = tue_out;
+        this.wed_out = wed_out;
+        this.thu_out = thu_out;
+        this.fri_out = fri_out;
+        this.sat_out = sat_out;
+
+
+    }
+
+    public Time(){}
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSun_in() {
+        return sun_in;
+    }
+    public void setSun_in(String sun_in) {
+        this.sun_in = sun_in;
+    }
+
+    public String getMon_in() {
+        return mon_in;
+    }
+    public void setMon_in(String mon_in) {
+        this.mon_in = mon_in;
+    }
+
+    public String getTue_in() {
+        return tue_in;
+    }
+    public void setTue_in(String tue_in) {
+        this.tue_in = tue_in;
+    }
+
+    public String getWed_in() {
+        return wed_in;
+    }
+    public void setWed_in(String wed_in) {
+        this.wed_in = wed_in;
+    }
+
+    public String getThu_in() {
+        return thu_in;
+    }
+    public void setThu_in(String thu_in) {
+        this.thu_in = thu_in;
+    }
+
+    public String getFri_in() {
+        return fri_in;
+    }
+    public void setFri_in(String fri_in) {
+        this.fri_in = fri_in;
+    }
+
+    public String getSat_in() {
+        return sat_in;
+    }
+    public void setSat_in(String sat_in) {
+        this.sat_in = sat_in;
+    }
+
+    public String getSun_out() {
+        return sun_out;
+    }
+    public void setSun_out(String sun_out) {
+        this.sun_out = sun_out;
+    }
+
+    public String getMon_out() {
+        return mon_out;
+    }
+    public void setMon_out(String mon_out) {
+        this.mon_out = mon_out;
+    }
+
+    public String getTue_out() {
+        return tue_out;
+    }
+    public void setTue_out(String tue_out) {
+        this.tue_out = tue_out;
+    }
+
+    public String getWed_out() {
+        return wed_out;
+    }
+    public void setWed_out(String wed_out) {
+        this.wed_out = wed_out;
+    }
+
+    public String getThu_out() {
+        return thu_out;
+    }
+    public void setThu_out(String thu_out) {
+        this.thu_out = thu_out;
+    }
+
+    public String getFri_out() {
+        return fri_out;
+    }
+    public void setFri_out(String fri_out) {
+        this.fri_out = fri_out;
+    }
+
+    public String getSat_out() {
+        return sat_out;
+    }
+    public void setSat_out(String sat_out) {
+        this.sat_out = sat_out;
+    }
+}
